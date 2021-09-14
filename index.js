294134767a2062d3704c56000770fc91682f80f9
sdefreese <samanthadefreese1@gmail.com>
2021-09-13 22:44:07 -0700

    commit tree 9d2b55395078ed5c9960746bdce42f2fef2cdc53

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const newDrivers = drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+  return newDrivers;
+}
+
+function fuzzyMatch(drivers, letters) {
+  const startsWith = drivers.filter(
+    (driver) => driver.toLowerCase()[0] === letters.toLowerCase()[0]
+  );
+  return startsWith;
+}
+
+function matchName(drivers, names) {
+  const nameMatch = drivers.filter(
+    (driver) => driver["name"].toLowerCase() === names.toLowerCase()
+  );
+  return nameMatch;
+}
+
+//match passed in name
+
+// returns mtaching drivers if case does not match but letters do
+
+//returns empty array if no match
